1b1c819b60c256f91dd061bbbf1a77c68c55898f
HMETAO <1430271732@qq.com>
2022-09-27 21:51:12 +0800

    fix(config): 修复objectmapper序列化问题

--- src/main/java/com/hmetao/code_dictionary/config/BeanInitConfig.java
+++ src/main/java/com/hmetao/code_dictionary/config/BeanInitConfig.java
@@ -1,6 +1,8 @@
 package com.hmetao.code_dictionary.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,6 +10,9 @@ import org.springframework.context.annotation.Configuration;
 public class BeanInitConfig {
     @Bean
     public ObjectMapper objectMapper() {
-        return new ObjectMapper();
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        objectMapper.registerModule(new JavaTimeModule());
+        return objectMapper;
     }
 }
